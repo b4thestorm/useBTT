82563932688fa56f2f19ef0fd59d1b4835822dc9
arnoldsanders <arnoldsander@gmail.com>
2025-01-11 16:22:09 -0500

    animated style

---
+++ src/app/components/carousel.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import * as motion from "motion/react-client"
+import Image from 'next/image'
+import { Box } from '@mui/material';
+
+export default function Carousel ({reverse}: {reverse: boolean}): React.JSX.Element {
+
+    const imgs = [ 
+        {id: 1, src: '/assets/bkylb0.jpg', alt: 'bkylb'}, 
+        {id: 2, src: '/assets/coffee-shop0.jpg', alt: 'coffee shop'},
+        {id: 3, src: '/assets/harlem-renaissance_0.jpg', alt: 'harlem renaissance'}
+    ]
+      
+    return (
+        <Box style={{overflowX: 'clip', overflowY: 'clip', width: '100%', height: '100%', display: 'flex', flexDirection: 'row'}}>
+            <motion.div
+            initial={{ x: reverse ? "0%" : "0%" }}
+            animate={{ 
+                x: reverse ? "100%" : "-100%"
+            }}
+            transition={{ duration: 8, delay: 1, ease: "linear", repeat: Infinity, repeatType: "loop" }}
+            style={{
+                display: 'flex',
+                flexDirection: 'row',
+                gap: '10px'
+            }}
+        >
+            {imgs.map((photo) => {
+                return <Image key={photo.id} src={photo.src} alt={''} width={150} height={150}/>
+            })}
+        </motion.div>
+        <motion.div
+            initial={{x: reverse ? "-300%" : "1%" }}
+            animate={{ 
+                x: reverse ? "0%" : "-100%"
+            }}
+            transition={{ duration: 8, delay: 1, ease: "linear", repeat: Infinity, repeatType: "loop" }}
+            style={{
+                display: 'flex',
+                flexDirection: 'row',
+                gap: '10px'
+            }}
+        >
+            {imgs.map((photo) => {
+                return <Image key={photo.id} src={photo.src} alt={''} width={150} height={150}/>
+            })}
+        </motion.div>
+        </Box>
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,32 @@
 import styles from "./page.module.css";
+import Carousel from "./components/carousel";
+import * as motion from "motion/react-client"
+
 // import IconButton from '@mui/material/IconButton';
 // import AlternateEmailIcon from '@mui/icons-material/AlternateEmail';
 
 export default function Home() {
   return (
     <div className={styles.page}>
+         <h1>Better Today Tech</h1>
       <main className={styles.main}>
-        <h1>Better Today Tech</h1>
+        <div className={styles.wall}>
+          <Carousel reverse={false}/>
+          <Carousel reverse={true}/>
+          <Carousel reverse={false}/>
+        </div>
         <div className={styles.about}>
-          <p>
+          <motion.p
+              initial={{ opacity: 0 }}
+              animate={{ 
+              opacity: 1
+          }}>
             BTT is a small digital consultancy 
             specializing in crafting modern
             <br/>
             digital experiences
-          </p>
+          </motion.p>
         </div>
-     
-
-
       </main>
       <footer className={styles.footer}>
         <p>© 2025 UseBTT</p>
