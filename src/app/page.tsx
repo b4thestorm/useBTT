148209808c5cdb868107635ab16479f4ce012aac
arnoldsanders <arnoldsander@gmail.com>
2025-01-11 23:14:40 -0500

    fix styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,27 +9,27 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <h1>Better Today Tech</h1>
+        <h1>
+          Better Today Tech
+        </h1>
         <div className={styles.wall}>
           <Carousel reverse={false}/>
           <Carousel reverse={true}/>
           <Carousel reverse={false}/>
         </div>
-        <div className={styles.about}>
-          <motion.p
+        {/* <div className={styles.about}> */}
+        <motion.p
               initial={{ opacity: 0 }}
               animate={{ 
               opacity: 1
-          }}
-          style={{ textAlign: 'center' }}
-          >
-
-            BTT is a small digital consultancy
-            <br/>
-            specializing in crafting modern
-            digital experiences
-          </motion.p>
-        </div>
+        }}
+        >
+          BTT is a small digital consultancy
+          <br/>
+          specializing in crafting modern
+          digital experiences
+        </motion.p>
+        {/* </div> */}
       </main>
       <footer className={styles.footer}>
         <p>© 2025 UseBTT</p>
